56b89dbf76d41bec94f84e60086ecca728d033fb
dyushakolesov <dyushakolesov@mail.ru>
2023-05-08 17:26:54 +0300

    basic tests done

--- tests/test_angle_model.py
+++ tests/test_angle_model.py
@@ -5,24 +5,29 @@ from PIL import Image
 import os
 import numpy as np
 from models.angle_model import AngleModel 
+#Укажи полный путь к проекту в .env файле
+from dotenv import load_dotenv
+dotenv_path = '.env'
+load_dotenv(dotenv_path)
+
+PATH_TO_SERVICE = os.getenv('PATH_TO_PROJ')
 
 ERR_STR = "No car on photo or bad angle, cannot change background"
-ANGLE_WEIGHTS = './weights/angle_classification.pt'
+ANGLE_WEIGHTS = PATH_TO_SERVICE + '/' + "models/weights/angle_classification.pt"
 BAD_ANGLE_IMAGES_LIST = ["opened_doors_image.jpg","bad_example.jpg"]
 
-PATH_TO_SERVICE = r"G:\Vlad\avito\car_segmentation_project\AAA-ML-Project\service\test_images"
-
+PATH_TO_TESTS = PATH_TO_SERVICE + '/' + 'tests/test_images'
 url = 'http://localhost:8080/change_background'
 
 
 @pytest.mark.parametrize("test_img_name", BAD_ANGLE_IMAGES_LIST)
 def test_check_bad_examples(test_img_name):
     
-    img_path = os.path.join(PATH_TO_SERVICE,test_img_name)
+    img_path = os.path.join(PATH_TO_TESTS,test_img_name)
     angle_model = AngleModel(ANGLE_WEIGHTS)
     image = Image.open(img_path)
     image_array = np.array(image)
     angle_is_good = angle_model.is_good_angle(image_array)
     
-    assert angle_is_good == False, f'Expected False, got {angle_model.predict(image_array)} class' 
+    assert angle_is_good == False, f"Expected is_good_angle=False, got {angle_model.predict(image_array)} class" 
 
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -14,12 +14,12 @@ async def test_index_page_contain_valid_multipart_form(client):
     assert form is not None
     assert form.attrs['method'].lower() == 'post'
     assert form.attrs['enctype'].lower() == 'multipart/form-data'
-    submit = form.find('input', {'type': 'submit'})
+    submit = form.find('button', {'type': 'submit'})
     assert submit is not None
 
 
 async def test_if_sent_faulty_image_then_error_appear_in_response(client):
-    faulty_image_path = 'tests/error.jpg'
+    faulty_image_path = 'tests/test_images/bad_example.jpg'
     form = FormData()
     form.add_field(
         name='image',
@@ -30,5 +30,5 @@ async def test_if_sent_faulty_image_then_error_appear_in_response(client):
     response = await client.post('/', data=form)
     text = (await response.text()).lower()
     assert response.status == HTTPStatus.OK, text
-    error = 'No car on photo or bad angle, cannot change background'
+    error = 'no car on photo or bad angle, cannot change background'
     assert error in text, text
